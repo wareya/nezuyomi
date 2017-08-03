d4b484a94b63186234e24bc61d321be7e2c5be73
wareya <wareya@gmail.com>
2017-08-03 16:50:41 -0400

    Optimize for size

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 #!bash
-g++ --std=c++14 main.cpp depends/gl3w.c depends/libglfw3.a -Iinclude -lopengl32 -lgdi32 -static -lstdc++fs -ggdb -mconsole -municode
+g++ --std=c++14 main.cpp depends/gl3w.c depends/libglfw3.a -Iinclude -lopengl32 -lgdi32 -static -lstdc++fs -ggdb -mconsole -mwindows -municode -Os -fdata-sections -ffunction-sections -Wl,--gc-sections -Wl,--strip-all
